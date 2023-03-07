f2aaf2eb1888a46b903a06b75515be63d8a8624b
SylivanKenobi <sypsilon@hotmail.com>
2023-03-07 13:16:26 +0100

    removed unused file and fix audio for pipewire

--- i3/audio.sh
+++ i3/audio.sh
@@ -2,33 +2,21 @@
 
 case "${1:-}" in
   (""|list)
-    pacmd list-sinks |
-      grep -E 'index:|name:'
+    pactl list short sinks
     ;;
   ([0-9]*)
-    echo switching default
-    pacmd set-default-sink $1 ||
-      echo failed
-    echo switching applications
-    pacmd list-sink-inputs |
-      awk '/index:/{print $2}' |
-      xargs -r -I{} pacmd move-sink-input {} $1 ||
-        echo failed
+    SINKS=$(pactl list short sinks | grep -v easyeffects)
+    NEW_SINK=$(echo "$SINKS" | sed "${1}q;d" | awk '{ print $2 }')
+    pactl set-default-sink "$NEW_SINK"
     ;;
   ("speackers"|"headset")
+    SINKS=$(pactl list short sinks | grep -v easyeffects)
     declare -A devices
-    devices=(["speackers"]="alsa_output.pci-0000_08_00.4.analog-stereo" ["headset"]="SteelSeries_Arctis_7")
-    echo switching default
-    sink_index=$(pacmd list-sinks | grep -B1 "${devices[$1]}" | awk '/index:/{print $NF}')
-    pacmd set-default-sink $sink_index ||
-      echo failed
-    echo switching applications
-    pacmd list-sink-inputs |
-      awk '/index:/{print $NF}' |
-      xargs -r -I{} pacmd move-sink-input {} $sink_index ||
-        echo failed
+    devices=(["speackers"]="alsa_output.pci-0000_08_00.4.analog-stereo" ["headset"]="alsa_output.usb-SteelSeries_Arctis_7_-00.analog-stereo")
+    pactl set-default-sink "${devices[$1]}"
     ;;
   (*)
     echo "Usage: $0 [|list|<sink name to switch to>]"
     ;;
 esac
+cd 
